f67a93f4d3fbaa027db0d4e61dd6749aedb070d7
Jackson Ho <mintri1199@gmail.com>
2020-11-08 01:41:55 -0500

    [Added] initial project set up

---
+++ challenge/challenge/challengeApp.swift
@@ -0,0 +1,20 @@
+//
+//  challengeApp.swift
+//  challenge
+//
+//  Created by Jackson Ho on 11/8/20.
+//
+
+import SwiftUI
+
+@main
+struct challengeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
